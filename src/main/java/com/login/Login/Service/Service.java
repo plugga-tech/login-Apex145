4ce554782f7b9a237895ce4f48d518e72f1adfb3
Apex145 <williamdi68@gmail.com>
2025-12-29 12:25:23 +0100

    Added folders and java files

---
+++ src/main/java/com/login/Login/Service/Service.java
@@ -0,0 +1,5 @@
+package com.login.Login.Service;
+
+public class Service {
+    
+}
---
+++ src/main/java/com/login/Login/User/User.java
@@ -0,0 +1,78 @@
+package com.login.Login.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String email;
+    private String firstname;
+    private String lastname;
+    private String username;
+    private String password;
+
+    public User() {}
+
+    public User(String email, String firstname, String lastname, String username, String password) {
+        this.email = email;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
